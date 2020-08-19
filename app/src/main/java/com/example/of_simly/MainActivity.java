de9191cfff311bbe14e47c7b44e1269906cda65f
ldhch2 <ldhch2@naver.com>
2020-08-19 23:34:58 +0900

    200819_at_veradi
    title 디자인

--- app/src/main/java/com/example/of_simly/MainActivity.java
+++ app/src/main/java/com/example/of_simly/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.of_simly;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView title = (ImageView) findViewById(R.id.gif_image);
+        Glide.with(this).load(R.drawable.title).into(title);
+
     }
 
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alBuilder = new AlertDialog.Builder(this);
+        alBuilder.setMessage("Are you sure you want to exit?");
+
+        alBuilder.setPositiveButton("okay", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                finish();
+            }
+        });
+
+        alBuilder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                return;
+            }
+        });
+        alBuilder.setTitle("Exit");
+        alBuilder.show();
+    }
 }
